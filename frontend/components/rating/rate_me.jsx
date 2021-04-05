f319e350d2ecdb47ab16817a3c192f0d848bd6d1
David <david.yang026@gmail.com>
2021-04-05 14:51:24 -0700

    included rating component for each chef

---
+++ frontend/components/rating/rate_me.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {IoFastFoodSharp} from 'react-icons/io5';
+
+class RateMe extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state ={
+            rating: null,
+            hover: null
+        };
+    }
+    
+    setRating(newValue){
+        this.setState ({ rating: newValue})
+    }
+
+    setHover(newValue){
+        this.setState ({ hover: newValue})
+    }
+
+    render() {
+        return(
+            <div>
+                {[...Array(5)].map((item, i) => {
+                    const ratingValue = i + 1;
+
+                    return (
+                        <label>
+                            <input 
+                                type="radio"
+                                style={{display:'none'}}
+                                value = {ratingValue}
+                                onClick = {() => this.setRating(ratingValue)}
+                            />
+
+
+                        
+                            <IoFastFoodSharp 
+                                size={50}
+                                color={ratingValue <= (this.state.hover ||this.state.rating) ? "ffc107" : "#e4e5e9"}
+                                onMouseEnter={()=> this.setHover(ratingValue)}
+                                onMouseExit={()=> this.setHover(null)}
+                                />
+                        </label>
+                    )
+
+                } )}
+            </div>
+                    
+
+        )
+        }
+}
+
+
+export default RateMe;
