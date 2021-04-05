4c27055accd7b92079de474338042e713b2305b2
David <david.yang026@gmail.com>
2021-04-04 21:27:26 -0700

    fixed create chef button

---
+++ frontend/components/chefs/chef_form.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class ChefForm extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = this.props.chef;
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  update(field){
+ 
+    return e => {
+
+      this.setState({[field]: e.currentTarget.value});
+    }
+  }
+
+  handleSubmit(){
+
+    this.props.action(this.state)
+  }
+  render (){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <h1> {this.props.formType} </h1>
+        <label>Name
+          <input
+            type="text"
+            value={this.state.name}
+
+            onChange={this.update('name')}
+          />
+        </label>
+        <label>location
+          <input
+            type="text"
+            value={this.state.location}
+            onChange={this.update('location')}
+          />
+        </label>
+        <label>About the Chef
+          <textarea
+            value={this.state.bio}
+            onChange={this.update('bio')}
+          />
+        </label>
+        <input type="submit" value={this.props.formType} />
+      </form>
+    );
+  }
+}
+export default ChefForm;
---
+++ frontend/components/chefs/create_chef_form_container.jsx
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import ChefForm from './chef_form';
+import { createChef } from '../../actions/chef_actions';
+
+const mapStateToProps = state => ({
+    chef: {
+        // can be empty strings so it acts as a placeholder
+        location: '',
+        bio: '',
+        name: '',
+      },
+      formType: "Create Chef"
+    });
+    
+    const mapDispatchToProps = dispatch => ({
+      // remember it's shared b/t createchef and editchef containers
+      // takes in a chef object and dispatches the createChef action with the chef object
+        // invoked
+
+      action: chef => dispatch(createChef(chef))
+    });
+    
+export default connect(mapStateToProps, mapDispatchToProps)(ChefForm);
