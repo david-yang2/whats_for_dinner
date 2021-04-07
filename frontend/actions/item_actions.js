d83a981da093b572a587ead60853c74b7f4e7d4e
David <david.yang026@gmail.com>
2021-04-07 14:03:45 -0700

    POJOs and Thunk actions for fetching chef's items and all items

---
+++ frontend/actions/item_actions.js
@@ -0,0 +1,33 @@
+import * as ItemApiUtil from "../util/item_api_util";
+
+// Reg Action Creators
+export const RECEIVE_ALL_ITEMS = 'RECEIVE_ALL_ITEMS';
+export const RECEIVE_CHEFS_ITEMS = "RECEIVE_CHEFS_ITEMS";
+
+const receiveItems = items => {
+    return{
+        type: RECEIVE_ALL_ITEMS,
+        items
+    }
+};
+
+const receiveChefsItems = chefItems => {
+    return{
+        type: RECEIVE_CHEFS_ITEMS,
+        chefItems
+    }
+};
+
+
+
+// THUNK action creators
+
+export const fetchItems = () => dispatch => {
+    return ItemApiUtil.fetchItems()
+    .then(items => dispatch(receiveItems(items)))
+};
+
+export const fetchChefsItems = chefId => dispatch => {
+    return ItemApiUtil.fetchChefsItems(chefId)
+    .then(chefItems => dispatch(receiveChefsItems(chefItems)))
+};
