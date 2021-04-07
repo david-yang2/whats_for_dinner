7f38eaa953e591db4506409502817e101992e626
David <david.yang026@gmail.com>
2021-04-07 13:01:09 -0700

    included price column and seeded db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_031527) do
+ActiveRecord::Schema.define(version: 2021_04_07_195207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_04_07_031527) do
     t.string "name", null: false
     t.text "description", null: false
     t.string "spiciness", null: false
+    t.float "price", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,8 @@ chef6 = Chef.create(location: "Sunset", bio: "We help make everyday delicious", 
 #     chef_id: , 
 #     name:"", 
 #     description:"", 
-#     spiciness: ""
+#     spiciness: "",
+#     price: 
 # )
 Item.destroy_all
 
@@ -39,47 +40,54 @@ item1 = Item.create(
     chef_id: chef1.id, 
     name:"Pho", 
     description:"Vietnamese soup consisting of broth, rice noodles, herbs, and meat, sometimes chicken", 
-    spiciness: "Medium"
+    spiciness: "Medium",
+    price: 9.99
     )
 
 item2 = Item.create(
     chef_id: chef3.id, 
     name:"Hamburger", 
     description:"Sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.", 
-    spiciness: "Non-Spicy"
+    spiciness: "Non-Spicy",
+    price: 12.99
     )
     
 item3 = Item.create(
     chef_id: chef1.id, 
     name:"Pineapple Fried Rice", 
     description:"Thai-style sweet and spicy pineapple fried rice with red bell pepper, cashews and cilantro.", 
-    spiciness: "Mild"
+    spiciness: "Mild",
+    price: 13.99
 )
 
 item4 = Item.create(
     chef_id: chef5.id, 
     name:"Crazy Roll", 
     description:"deep-fried California Roll served with eel sauce", 
-    spiciness: "Medium"
+    spiciness: "Medium",
+    price: 15.99
 )
 
 item5 = Item.create(
     chef_id: chef3.id, 
     name:"Alaskan Roll", 
     description:"smoked salmon, asparagus and avocado", 
-    spiciness: "Non-Spicy"
+    spiciness: "Non-Spicy",
+    price: 9.99
 )
 
 item6 = Item.create(
     chef_id: chef2.id, 
     name:"Unagi Roll", 
     description:"freshwater eel and avocado on California Roll topped with sweet sauce", 
-    spiciness: "Non-Spicy"
+    spiciness: "Non-Spicy",
+    price: 6.99
 )
 
 item7 = Item.create(
     chef_id: chef6.id, 
     name:"MARION’S BEST BEEF STROGANOFF", 
     description:"Russian dish of sautéed pieces of beef served in a sauce with smetana.", 
-    spiciness: "Spicy"
+    spiciness: "Spicy",
+    price: 16.99
 )
