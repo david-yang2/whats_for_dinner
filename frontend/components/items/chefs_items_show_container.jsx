7ec135112539381f5e6b5adeb8125195fcea8974
David <david.yang026@gmail.com>
2021-04-07 20:37:00 -0700

    mapped the sliced state to route

--- frontend/components/items/chefs_items_show_container.jsx
+++ frontend/components/items/chefs_items_show_container.jsx
@@ -3,8 +3,11 @@ import ChefsItemsShow from './chefs_items_show';
 import { fetchChefsItems } from '../../actions/item_actions';
 
 
-const mapStateToProps = state =>{
-  return state
+const mapStateToProps = (state) =>{
+  // chefItems already includes a specific slice of the state
+    // belonging to the chef. The id of 28 is fetched from /api/chefs/28/item
+    // from item_api_util -> fetchChefsItems
+  return {items: state.items}
 };
 
 const mapDispatchToProps = dispatch => ({
