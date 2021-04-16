155078e2b9e2028abecbb1784dd5284e9ebc8e68
David <david.yang026@gmail.com>
2021-04-16 15:52:13 -0700

    updating layout of home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import "./home.css";
+
+
 
  const Home = () => {
     return (
-        <div className="home">
-            <h1>Can't decide on dinner?</h1>
-            <img src='/images/home/homepagelogo.jpeg' alt="" />
+        <div>
+            <div className="home">
+                <h1 className="hometitle">What's for dinner</h1>
+            </div>
         </div>
+
+
     )
 }
 
