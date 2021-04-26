8412ef46d3fca266c6ae0167c3f3ae4dcad65cb4
David <david.yang026@gmail.com>
2021-04-26 12:40:30 -0700

    initial commit

---
+++ frontend/components/cart/cart_items.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Cartitems extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state = this.props.item
+    }
+
+    render(){
+        return null
+    }
+
+
+}
+
+export default Cartitems;
