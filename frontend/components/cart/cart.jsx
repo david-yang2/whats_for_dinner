9604b735bbc05ddac7582ac90d415befbab0b049
David <david.yang026@gmail.com>
2021-06-14 17:21:46 -0700

    included DeliveryInstructions component

--- frontend/components/cart/cart.jsx
+++ frontend/components/cart/cart.jsx
@@ -1,42 +1,30 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import Cartitems from "./cart_items";
+import DeliveryInstructions from "./deliver_instructions"
 
 
 class Cart extends React.Component {
     constructor(props){
         super(props);
 
-        this.updateReview = this.updateReview.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);  
+        this.updateDeliveryInstructions = this.updateDeliveryInstructions.bind(this);
     }
 
     componentDidMount(){
         this.props.fetchCurrentUser(this.props.session.currentUser.id)
         if (!this.props.session.currentUser) return null
         this.props.getUsersCart(this.props.session.currentUser.id)
-        
-    }
-
-    update(field){
-        return e => {
-          this.setState({[field]: e.currentTarget.value});
-        }
-      }
-
-    handleSubmit(){
-        return this.props.updateInstructions({
-                                            id: this.props.session.currentUser.id,
-                                            deliveryInstructions: this.state.deliveryInstructions})
 
     }
 
-    updateReview(updatedReview){
-        this.props.updateCartReview(updatedReview)
+    updateDeliveryInstructions(updatedInstructions){
+        this.props.updateInstructions(updatedInstructions)
     }
 
     render(){
-        if (!this.props.cartitems || !this.props.user) return null
+        if (!this.props.cartitems) return null
+        if (Object.values(this.props.user).length === 0 ) return null
 
 
         // map through cartitems and create array with their prices
@@ -44,7 +32,7 @@ class Cart extends React.Component {
 
         // get sum and set to 2 decimal places
         let subTotal = subArr.reduce((a, b) => a + b, 0).toFixed(2)
-        this.state = this.props.user
+
         return (
             <div className="cart-container">
                 <div className="cart-header">
@@ -56,28 +44,19 @@ class Cart extends React.Component {
                                                             <Cartitems 
                                                                 key={idx}
                                                                 item = {item} 
-                                                                // updateReview = {this.updateReview}
                                                                 />
                                                                 )}
                     </div>
                     <div className="check-out">
+                        {/* get subtotal of current cart and render delivery instructions form */}
                         <h2>Your subtotal is: ${subTotal}</h2>
-                        <form className="instructions-form"
-                                onSubmit={this.handleSubmit}>
-                            <textarea className="instructions-box"
-                                value={this.state.deliveryInstructions}
-                                placeholder="Would you like to provide any delivery instructions?"
-                                onChange = {this.update('deliveryInstructions')}
-                            />
-                            <input type="submit" value="Update Instructions" />
-                        </form>
+                        <DeliveryInstructions user={this.props.user} 
+                                              updateDeliveryInstructions = {this.updateDeliveryInstructions}/>
+
                     </div>
                 </div>
             </div>
         )}
-
-
-    // }
 
 }
 
@@ -88,25 +67,41 @@ export default Cart;
 
 
 
+    {/* <form className="instructions-form"
+            onSubmit={this.handleSubmit}>
+        <textarea className="instructions-box"
+            value={this.state.deliveryInstructions}
+            // placeholder="Would you like to provide any delivery instructions?"
+            onChange = {this.update('deliveryInstructions')}
+        />
+        <input className="update-btn" type="submit" value="Update Delivery Instructions" />
+    </form> */}
 
 
 
-        // if (this.props.fromItemContainer){
-        //    return(
-        //        <div >
-        //             <div className="cart-container">
-        //                 {Object.values(this.props.cartitems).map((item,idx)=> 
-        //                                         <h3 key={idx}> {item.name}</h3>)}
-        //             </div>
-        //             <Link to="/cart">Check out your saved items!</Link>
-        //         </div>
-        //         )
-        //     } else {
+    // handleSubmit(){
+    //     this.props.updateInstructions({
+    //                                         id: this.props.session.currentUser.id,
+    //                                         deliveryInstructions: this.state.deliveryInstructions})
+
+    // }
+
+    // if (this.props.fromItemContainer){
+    //    return(
+    //        <div >
+    //             <div className="cart-container">
+    //                 {Object.values(this.props.cartitems).map((item,idx)=> 
+    //                                         <h3 key={idx}> {item.name}</h3>)}
+    //             </div>
+    //             <Link to="/cart">Check out your saved items!</Link>
+    //         </div>
+    //         )
+    //     } else {
 
 
 
-        // updateInstructions(instructions){
-        //     this.props.updateDeliveryInstructions(instructions)
-        // }
+    // updateInstructions(instructions){
+    //     this.props.updateDeliveryInstructions(instructions)
+    // }
 
-        // this.updateInstructions = this.updateInstructions.bind(this)
+    // this.updateInstructions = this.updateInstructions.bind(this)
