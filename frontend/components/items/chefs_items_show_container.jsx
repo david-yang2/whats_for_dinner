5bc09da4240182dac695be1fd84a7f89dbbe60c4
David <david.yang026@gmail.com>
2021-06-16 14:01:50 -0700

    refactored mapStateToProps

--- frontend/components/items/chefs_items_show_container.jsx
+++ frontend/components/items/chefs_items_show_container.jsx
@@ -2,17 +2,20 @@ import { connect } from 'react-redux';
 import ChefsItemsShow from './chefs_items_show';
 import { fetchChefsItems } from '../../actions/item_actions';
 import { fetchChef } from '../../actions/chef_actions';
+import { updateCart } from "../../actions/user_actions";
 
 
 const mapStateToProps = (state, ownProps) =>({
   chef: state.chefs[ownProps.match.params.id],
-  items: state.items
+  items: state.items,
+  session: state.session
 })
 
 
 const mapDispatchToProps = dispatch => ({
   fetchChef: id => dispatch(fetchChef(id)),
-  fetchChefsItems: id => dispatch(fetchChefsItems(id))
+  fetchChefsItems: id => dispatch(fetchChefsItems(id)),
+  updateCart: user => dispatch(updateCart(user))
 });
 
 export default connect(mapStateToProps,mapDispatchToProps)(ChefsItemsShow);
