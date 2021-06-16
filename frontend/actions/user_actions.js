6461eeade896a37aa98266b1b5cb43ed32c50543
David <david.yang026@gmail.com>
2021-06-16 13:59:12 -0700

    refactored actions

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,22 +1,24 @@
 import * as UserApiUtil from "../util/user_api_util";
 
 export const CURRENT_USER = "CURRENT_USER";
-export const UPDATE_CART = "UPDATE_CART";
+export const UPDATE_INSTRUCTIONS = "UPDATE_INSTRUCTIONS";
+export const UPDATE_REVIEW = "UPDATE_REVIEW";
 
 
-const getCurrentUser = user => {
-    return{
+const getCurrentUser = user => ({  
     type: CURRENT_USER,
     user
-    }
-};
+})
 
-const updateUsersCart = updatedCart => {
-    return{
-    type: UPDATE_CART,
-    updatedCart
-}
-};
+const updatedInstructions = user => ({        
+    type: UPDATE_INSTRUCTIONS,
+    user
+})
+
+const updateReview = updatedReview => ({
+    type: UPDATE_REVIEW,
+    updatedReview
+})
 
 
 
@@ -25,8 +27,19 @@ export const fetchCurrentUser = userId => dispatch => {
     .then(receivedUser => dispatch(getCurrentUser(receivedUser)))
 };
 
+export const updateInstructions = instructions => dispatch => {
+    return UserApiUtil.patchInstructions(instructions)
+    .then(updated => dispatch(updatedInstructions(updated)))
+};
+
+
+export const updateCartReview = item => dispatch => 
+{ 
+    return UserApiUtil.updateCart(item)
+    .then(updatedCart => dispatch(updateReview(updatedCart)))
+}
+
 export const updateCart = item => dispatch => {
     return UserApiUtil.postCart(item)
     // .then(updatedCart => dispatch(updateUsersCart(updatedCart)))
 };
-
