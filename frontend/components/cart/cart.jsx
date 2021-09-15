8ff5c5a1500caf0ea1e0043eeee3ee197e69702d
David <david.yang026@gmail.com>
2021-09-15 11:45:25 -0700

    removed duplicate items from cart

--- frontend/components/cart/cart.jsx
+++ frontend/components/cart/cart.jsx
@@ -23,28 +23,33 @@ class Cart extends React.Component {
     }
 
     render(){
-        if (!this.props.cartitems) return null
-        if (Object.values(this.props.user).length === 0 ) return null
-
+        if (Object.values(this.props.cartitems).length === 0 || Object.values(this.props.user).length === 0 ) return null
 
         // map through cartitems and create array with their prices
         let subArr = Object.values(this.props.cartitems).map((item) => item.price)
-
         // get sum and set to 2 decimal places
         let subTotal = subArr.reduce((a, b) => a + b, 0).toFixed(2)
 
+        // create a counts object to keep track of qunatity of same items
+        const counts = {}
+        Object.values(this.props.cartitems).forEach(x => { counts[x.name] = (counts[x.name] || 0) + 1; });
+        
+
+        // get unique item objects from array of cartitems
+        const uniqueItems = [...this.props.cartitems.reduce((map,obj) => map.set(obj.name, obj), new Map()).values()]
+        
         return (
             <div className="cart-container">
                 <div className="cart-header">
-                    <h2>Items in your cart:</h2>
+                    <h2>There are {this.props.cartitems.length} items in your cart :</h2>
                 </div>
                 <div className="cart-info">
                     <div className="cart-items-container">
-                        {Object.values(this.props.cartitems).map((item,idx)=>
+                        {Object.values(uniqueItems).map((item,idx)=>
                                                             <Cartitems 
                                                                 key={idx}
                                                                 item = {item} 
-                                                                />
+                                                                counts={counts}/>
                                                                 )}
                     </div>
                     <div className="check-out">
