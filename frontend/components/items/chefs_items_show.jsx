57ba8dce62600b20e0291eec7288590736d2c758
David <david.yang026@gmail.com>
2021-09-15 18:11:40 -0700

    refactored and passed in ChefsItems component

--- frontend/components/items/chefs_items_show.jsx
+++ frontend/components/items/chefs_items_show.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+
 import { Link } from 'react-router-dom';
+import ChefsItems from "./chefs_items"
 
 //chefs/28/items
 class ChefsItemsShow extends React.Component{
 
   constructor(props){
     super(props)
-    
+
+
     this.addToCart = this.addToCart.bind(this)
   }
 // this will invoke the connect function
@@ -43,29 +46,12 @@ class ChefsItemsShow extends React.Component{
         </div>
         <div className="chefs-items-list">
             <h3>Here's a list of {chef.name}'s dishes:</h3>
-            {Object.values(items).map(item=>  <div className="chefs-item" key={item.id}>
-                                                <div className="chef-item-header">
-                                                  <img className="item-img" src={item.imagepath} alt="" />
-                                                </div>
-                                                <div className="chef-item-content">
-                                                  <h3>{item.name}</h3>
-                                                  <h5>Description: {item.description.toLowerCase()}</h5>
-                                                  <div className="chef-checkout">
-                                                    <h4>Price: {item.price}</h4>
-                                                    {/* display save item button if a user is logged in */}
-                                                    {this.props.session.currentUser ?
-                                                    <button className="item-btn"
-                                                            onClick={()=>this.addToCart({
-                                                                                        name:  item.name,
-                                                                                        description: item.description,
-                                                                                        price: item.price,
-                                                                                        imagepath: item.imagepath
-                                                                                      })}>Save Item!</button>
-                                                    :
-                                                    null}
-                                                  </div>
-                                                </div>
-                                              </div> 
+            {/* Map through chef's items and create a menu on the side
+                allow users to add an item to their cart */}
+            {Object.values(items).map(item=>  <ChefsItems key={item.id}
+                                                          item={item}
+                                                          currentUser={this.props.session.currentUser}
+                                                          addToCart={this.addToCart}/>
             )}
         </div>
       </div>
@@ -74,3 +60,55 @@ class ChefsItemsShow extends React.Component{
 }
 
 export default ChefsItemsShow;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="chefs-item" key={item.id}>
+    <div className="chef-item-header">
+      <img className="item-img" src={item.imagepath} alt="" />
+    </div>
+    <div className="chef-item-content">
+      <h3>{item.name}</h3>
+      <h5>Description: {item.description.toLowerCase()}</h5>
+      <div className="chef-checkout">
+        <h4>Price: {item.price}</h4>
+        display save item button if a user is logged in
+        {this.props.session.currentUser ?
+        <button className="item-btn"
+                onClick={()=>{this.addToCart({
+                                            name:  item.name,
+                                            description: item.description,
+                                            price: item.price,
+                                            imagepath: item.imagepath
+                                          });
+                                this.showPopup(true)}}>Save Item!</button>
+        :
+        null}
+      <Popup trigger={this.state.btnpopup}
+        showPopup={this.showPopup}>
+      
+        <h4>You've succesfully added {item.name} to your cart</h4>
+      </Popup>
+      </div>
+
+    </div>
+  </div>  */}
